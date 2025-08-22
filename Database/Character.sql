aa48621f8bdd05893ac0c8bfb4e111b0d4435054
shakana0 <shakanajamal3@gmail.com>
2025-08-22 18:45:36 +0200

    Clean up tracked files and apply .gitignore rules

---
+++ Database/Character.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Character]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(100),
+    [CurseLevel] INT,
+    [VibeType] NVARCHAR(50),
+    [BackStory] NVARCHAR(MAX),
+    [Strength] INT,
+    [Agility] INT,
+    [Intelligence] INT,
+    [Luck] INT
+)
