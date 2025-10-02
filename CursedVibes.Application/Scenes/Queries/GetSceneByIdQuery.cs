52b0f526c6ff9b96017b9c10facec42fa723c65f
shakana0 <shakanajamal3@gmail.com>
2025-10-02 17:00:21 +0200

    feat(application): define GetSceneByIdQuery to request scene data via MediatR pipeline

---
+++ CursedVibes.Application/Scenes/Queries/GetSceneByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using CursedVibes.Application.Characters.Dtos;
+using MediatR;
+
+namespace CursedVibes.Application.Scenes.Queries
+{
+    public class GetSceneByIdQuery: IRequest<SceneDto>
+    {
+        public string SceneId { get; set; } = string.Empty;
+
+        public GetSceneByIdQuery(string sceneId)
+        {
+            SceneId = sceneId;
+        }
+    }
+}
