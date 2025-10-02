5f342217fbb4276035f4f04f6e4e8716907b532a
shakana0 <shakanajamal3@gmail.com>
2025-10-02 17:01:29 +0200

    feat(application): add GetSceneByIdQueryValidator to validate scene query input

---
+++ CursedVibes.Application/Scenes/Queries/GetSceneByIdQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace CursedVibes.Application.Scenes.Queries
+{
+    public class GetSceneByIdQueryValidator: AbstractValidator<GetSceneByIdQuery>
+    {
+        public GetSceneByIdQueryValidator() 
+        {
+            RuleFor(x => x.SceneId)
+            .NotEmpty().WithMessage("SceneId is required.")
+            .MinimumLength(3).WithMessage("SceneId must be at least 3 characters.")
+            .MaximumLength(100).WithMessage("SceneId must be less than 100 characters.")
+            .Matches("^[a-zA-Z0-9_-]+$").WithMessage("SceneId contains invalid characters.");
+        }
+    }
+}
