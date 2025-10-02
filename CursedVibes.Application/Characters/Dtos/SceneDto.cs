9098339959b1545bdf648f15e3bdf63f93a57ffd
shakana0 <shakanajamal3@gmail.com>
2025-10-02 16:57:03 +0200

    feat(application): add SceneDto to represent scene data for transport and serialization

---
+++ CursedVibes.Application/Characters/Dtos/SceneDto.cs
@@ -0,0 +1,15 @@
+﻿using CursedVibes.Domain.ValueObjects;
+
+namespace CursedVibes.Application.Characters.Dtos
+{
+    public class SceneDto
+    {
+        public required string SceneId { get; set; }
+        public required string SceneType { get; set; }
+        public required string Chapter { get; set; }
+        public required string Background { get; set; }
+        public List<string> Narration { get; } = new List<string>();
+        public List<SceneChoices> Choices { get; } = new List<SceneChoices>();
+        public List<string> Effects { get; } = new List<string>();
+    }
+}
