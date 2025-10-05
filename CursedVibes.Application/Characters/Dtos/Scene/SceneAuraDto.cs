f8e336cc4a4de1325ec854a3c7fe7ddf7a461373
shakana0 <shakanajamal3@gmail.com>
2025-10-05 21:26:38 +0200

    Add: Created SceneAuraDto for clean separation between domain model and external representation

---
+++ CursedVibes.Application/Characters/Dtos/Scene/SceneAuraDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace CursedVibes.Application.Characters.Dtos.Scene
+{
+    public class SceneAuraDto
+    {
+        public string Background { get; set; } = string.Empty;
+        public string? AmbientAudio { get; set; }
+        public string? Music { get; set; }
+        public List<string>? Sfx { get; set; }
+    }
+
+}
