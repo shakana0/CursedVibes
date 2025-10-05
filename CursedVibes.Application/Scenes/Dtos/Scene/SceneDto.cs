719e192e0864453500882bd930c944d26820322d
shakana0 <shakanajamal3@gmail.com>
2025-10-05 21:38:15 +0200

    Refactor: Replaced nested value objects with DTOs in SceneDto to align with transport layer conventions

---
+++ CursedVibes.Application/Scenes/Dtos/Scene/SceneDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CursedVibes.Application.Scenes.Dtos.Scene
+{
+    public class SceneDto
+    {
+        public required string SceneId { get; set; }
+        public required string SceneType { get; set; }
+        public required string Chapter { get; set; }
+        public SceneAuraDto SceneAura { get; set; } = new SceneAuraDto();
+        public List<string> Narration { get; } = new List<string>();
+        public List<SceneChoicesDto> Choices { get; } = new List<SceneChoicesDto>();
+        public List<string> Effects { get; } = new List<string>();
+    }
+}
