b3cd69624f581e9f9e2e635d13754f2ef5eb6222
shakana0 <shakanajamal3@gmail.com>
2025-10-05 21:27:25 +0200

    Add: Created StatDeltaDto for clean separation between domain model and external representation

---
+++ CursedVibes.Application/Characters/Dtos/Scene/StatDeltaDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CursedVibes.Application.Characters.Dtos.Scene
+{
+    public class StatDeltaDto
+    {
+        public int? Health { get; set; }
+        public int? CurseLevel { get; set; }
+        public string? Status { get; set; }
+        public string? EmotionalState { get; set; }
+        public int? DesireLevel { get; set; }
+        public int? Corruption { get; set; }
+        public string? ChatTone { get; set; }
+        public Dictionary<string, int>? Trust { get; set; }
+    }
+}
