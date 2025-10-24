ab05c1f0a2d7b676b16c4951caf8177e450dbaeb
shakana0 <shakanajamal3@gmail.com>
2025-10-24 07:23:25 +0200

    feat: Add Player domain model

---
+++ CursedVibes.Domain/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using CursedVibes.Domain.ValueObjects;
+
+namespace CursedVibes.Domain.Player
+{
+    public class Player
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public PlayerNarrativeState PlayerNarrativeState { get; private set; } = new PlayerNarrativeState();
+
+        // Optional: future login support
+        public string? Email { get; private set; }
+        public string? Username { get; private set; }
+
+        // Optional: methods to link login later
+        public void Register(string email, string username)
+        {
+            Email = email;
+            Username = username;
+        }
+
+    }
+}
