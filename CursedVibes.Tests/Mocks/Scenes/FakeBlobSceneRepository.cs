97d4dc3ed5bd3189d7dfcd6b5ea349bed86f9a71
shakana0 <shakanajamal3@gmail.com>
2025-11-02 12:41:24 +0100

    test: Add FakeBlobSceneRepository to read content from json file

---
+++ CursedVibes.Tests/Mocks/Scenes/FakeBlobSceneRepository.cs
@@ -0,0 +1,18 @@
+﻿using CursedVibes.Domain.Interfaces;
+
+namespace CursedVibes.Tests.Mocks.Scenes
+{
+    public class FakeBlobSceneRepository : ISceneRepository
+    {
+        public Task<Stream> GetSceneAsync(string sceneId, CancellationToken cancellationToken)
+        {
+            var filePath = Path.Combine(AppContext.BaseDirectory, "Mocks", "Scenes", $"{sceneId}.json");
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"Scene '{sceneId}' not found at '{filePath}'.");
+
+            var stream = File.OpenRead(filePath);
+            return Task.FromResult<Stream>(stream);
+        }
+    }
+}
