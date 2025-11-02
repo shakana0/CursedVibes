9dc69355367071ac734a320fd6f371ba24ac952c
shakana0 <shakanajamal3@gmail.com>
2025-11-02 12:43:11 +0100

    test: Add unit test for get scene req with valid scene id

---
+++ CursedVibes.Tests/Application/ScenesTests/Queries/GetSceneByIdHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using CursedVibes.Application.Scenes.Dtos.Scene;
+using CursedVibes.Application.Scenes.Queries;
+using CursedVibes.Domain.Entities;
+using CursedVibes.Domain.Interfaces;
+using CursedVibes.Tests.Mocks.Scenes;
+using Moq;
+
+namespace CursedVibes.Tests.Application.ScenesTests.Queries
+{
+    public class GetSceneByIdHandlerTests
+    {
+        private readonly ISceneRepository _sceneRepository;
+        public GetSceneByIdHandlerTests()
+        {
+            _sceneRepository = new FakeBlobSceneRepository();
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidSceneIdQuery_ReturnsSceneDto()
+        {
+            var query = new GetSceneByIdQuery("scene1");
+            var mapperMock = new Mock<IMapper>();
+            mapperMock
+                .Setup(m => m.Map<SceneDto>(It.IsAny<Scene>()))
+                .Returns((Scene s) => SceneTestDataGenerator.CreateSceneDto(query.SceneId)
+            );
+
+            var handler = new GetSceneByIdHandler(_sceneRepository, mapperMock.Object);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.IsType<SceneDto>(result);
+            Assert.Equal("scene1", result.SceneId);
+            Assert.Collection(result.Narration,
+                line => Assert.Equal("It was a quiet morning...", line),
+                line => Assert.Equal("The sun rose slowly over the hills.", line)
+            );
+            Assert.Single(result.Choices);
+            Assert.Equal(10, result.Choices[0].StatImpact.CurseLevel);
+
+            mapperMock.Verify(m => m.Map<SceneDto>(It.IsAny<Scene>()), Times.Once);
+
+        }
+    }
+}
